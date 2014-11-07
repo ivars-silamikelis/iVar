b38071d8873b691c1fedc840e6f17dd445425ec6
Ivarz <ivarssila@inbox.lv>
2014-11-07 16:08:04 +0200

    added licence

--- iVar.pl
+++ iVar.pl
@@ -1,4 +1,23 @@
 #!/usr/bin/perl;
+
+#	 iVar filters vcf file by rs IDs and/or sample genotypes.
+#    Copyright (C) 2014 Ivars Silamikelis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#	 
+#	 If you have any questions or comments you can contact me at ivars.silamikelis@biomed.lu.lv
+
 use strict;
 use warnings;
 use Getopt::Long;
